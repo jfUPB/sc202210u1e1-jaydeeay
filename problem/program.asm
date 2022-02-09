33d1a1d3b4e38997f87ea30b01af0e35be15b2ed
jaydeeay <jarroyavep@outlook.com>
2022-02-08 21:27:40 -0500

    Integración del programa proyecto 4 fill

--- problem/program.asm
+++ problem/program.asm
@@ -1,6 +1,42 @@
 // JULIAN DAVID ARROYAVE PÉREZ
 // 391537
 // julian.arroyave@upb.edu.co
-(START)
-    @START
-    0;JMP
+
+@16384
+D=A
+@1
+M=D
+@1
+D=M
+@2 
+M=D
+@24576
+D=M
+@25
+D;JEQ
+@2
+D=M
+@24575
+D=D-A
+@4
+D;JGT
+@2
+A=M
+M=1
+@2
+M=M+1
+@12
+0;JMP
+@2
+D=M 
+@24575
+D=D-A
+@4
+D;JGT
+@2
+A=M 
+M=0
+@2
+M=M+1
+@25
+0;JMP
